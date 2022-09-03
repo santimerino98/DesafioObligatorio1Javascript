c55816b0db642982a6a9e4713148ae50e66d63da
santimerino98 <92310932+santimerino98@users.noreply.github.com>
2022-09-03 16:14:27 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
-function primo(){
-	let n=parseInt(prompt("Ingrese el número a testear:"));
-	let i=2;
-	while(i<n){
-		if(n%i==0){
-			alert("El número "+n+" no es primo");
-			break;}
-		else 
-			i++;
-	}
-	if(i==n)
+function esPrimo () {
+    let n1 = prompt ('Introduce un numero');
+    let i = 2;
+    primo = true;
+    
 
-    alert("El número "+n+" es primo");
+        while(i<n1){
+            if(n1 == 0 || n1 == 1 || n1 == 4 || n1%i==0){
+                primo=false;
+            }
+            i++;
+        }
+    
+
+    
+    if (primo==true){
+        alert('El numero '+n1+' es primo');
+    }
+    else{
+        alert('El numero '+n1+' no es primo');
+    }
 }
 
-primo();
+esPrimo();

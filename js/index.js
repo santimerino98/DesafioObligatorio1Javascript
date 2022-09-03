deda798bf4056631b0a7b3a01c0faa15bfcad02c
santimerino98 <92310932+santimerino98@users.noreply.github.com>
2022-09-03 15:18:12 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function primo(){
-	var n=parseInt(prompt("Ingrese el número a testear:"));
-	var i=2;
+	let n=parseInt(prompt("Ingrese el número a testear:"));
+	let i=2;
 	while(i<n){
 		if(n%i==0){
 			alert("El número "+n+" no es primo");
